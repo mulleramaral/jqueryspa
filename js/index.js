ba628d1f4e97416020ea6c61937fa1fd5529a052
Miiller Amaral <miiller@microwork.inf.br>
2017-09-27 11:36:32 -0300

    spa

--- js/index.js
+++ js/index.js
@@ -1,13 +1,9 @@
-$(function(){
-    $('#interno a').on('click',function(evt){
-        evt.preventDefault();
-        var url = $(this).attr('href');
-        $.get(url).done(function(data){
-            $('#conteudo').html(data);
-        }).fail(function(){
-            $('#conteudo').html('Página não encontrada');
-        });
-    });
-
-    $('#conteudo').load('pages/home.html');
+$(function() {
+    $(window).on('hashchange', hashchanged);
+    hashchanged();
 });
+
+function hashchanged() {
+    var hash = location.hash.replace('#', '') || 'pages/home';
+    $("#conteudo").load(hash + '.html');
+}
